9495ab4cac5c2c706c6a521089e19a7649b782be
bdoherty <bradleydoherty59@gmail.com>
2022-02-28 11:18:54 -0600

    Simple bootloader and VGA output

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,17 @@ pub extern  "C" fn _start() -> !{
    let vga_buffer = 0xb8000 as *mut u8;
 
    // Iterate over the bytes of the static Hello string
-   // Using enumerate 
+   // Using enumerate to get additional running var i
    for (i, &byte) in HELLO.iter().enumerate() {
-       unsafe {
-           *vga_buffer.offset(i as isize * 2) = byte;
-           *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-       }
+       // Unsafe because raw pointers are not provably valid
+       // but we know this is where the VGA buffer is 
+        unsafe {
+                // Offset method allows writing string byte from
+                // HELLO
+                *vga_buffer.offset(i as isize * 2) = byte;
+                // Use cyan (0xb) as color for VGA
+                *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            }
    }
 
    loop {}
