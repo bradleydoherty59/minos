13b5165b355f7f413efce04e8e07d33bc7eed04b
bdoherty <bradleydoherty59@gmail.com>
2022-03-05 01:12:47 -0600

    VGA and printing macros

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,19 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
-
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
+fn panic(info: &PanicInfo) -> !{
+    println!("{}", info);
     loop{}
 }
 
-static HELLO: &[u8] = b"Hello World! My name is Brad";
 
 #[no_mangle] // don't mangle the name of this function
 pub extern  "C" fn _start() -> !{
-    // Cast VGA buffer address into a raw pointer
-   let vga_buffer = 0xb8000 as *mut u8;
-
-   // Iterate over the bytes of the static Hello string
-   // Using enumerate to get additional running var i
-   for (i, &byte) in HELLO.iter().enumerate() {
-       // Unsafe because raw pointers are not provably valid
-       // but we know this is where the VGA buffer is 
-        unsafe {
-                // Offset method allows writing string byte from
-                // HELLO
-                *vga_buffer.offset(i as isize * 2) = byte;
-                // Use cyan (0xb) as color for VGA
-                *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-            }
-   }
-
-   loop {}
+    println!("Hello Brad!");
+    panic!("Some panic message");
+ loop {}
 }
